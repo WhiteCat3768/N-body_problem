0e167adf55fb35219c9351a5cf4fb24b891fe5af
Daniel <dany-pnm@yandex.com>
2025-11-30 17:40:38 +0300

    Finished RK4 explicit method.

--- nbody_simulation/src/n_body.py
+++ nbody_simulation/src/n_body.py
@@ -2,22 +2,17 @@ import sys
 from pathlib import Path
 
 # This line adds the real project root (the folder that contains both "main_folder" and "data")
-PROJECT_ROOT = Path(__file__).resolve().parents[1]   # ← two levels up from main.py
+PROJECT_ROOT = Path(__file__).resolve().parents[1]  # ← two levels up from main.py
 sys.path.insert(0, str(PROJECT_ROOT))
 
 from data.solar_system_data import *
 from data.initial_conditions import *
 
-#  from physics import *
-
 class Body:
     G = 6.674e-11
-    bodies = []
     dt = 1e+5
 
-    def __init__(self, name, trail_color, mass, x, y, z, speed_x, speed_y, speed_z):
-
-        Body.bodies.append(self)
+    def __init__(self, name, trail_color, mass, x, y, z, v_x, v_y, v_z):
 
         self.name = name
         self.trail_color = trail_color
@@ -25,13 +20,12 @@ class Body:
         self.x = x
         self.y = y
         self.z = z
-        self.speed_x = speed_x
-        self.speed_y = speed_y
-        self.speed_z = speed_z
-        self.trail = []
+        self.v_x = v_x
+        self.v_y = v_y
+        self.v_z = v_z
 
 
-def Register_body(name):
+def register_body(name):
 
     name = next((body[0] for body in bodies_data if body[0] == name), None)
     trail_color = next((body[1] for body in bodies_data if body[0] == name), None)
@@ -39,12 +33,11 @@ def Register_body(name):
     x = next((body[1] for body in bodies_conditions if body[0] == name), None)
     y = next((body[2] for body in bodies_conditions if body[0] == name), None)
     z = next((body[3] for body in bodies_conditions if body[0] == name), None)
-    speed_x = next((body[4] for body in bodies_conditions if body[0] == name), None)
-    speed_y = next((body[5] for body in bodies_conditions if body[0] == name), None)
-    speed_z = next((body[6] for body in bodies_conditions if body[0] == name), None)
-    trail = []
+    v_x = next((body[4] for body in bodies_conditions if body[0] == name), None)
+    v_y = next((body[5] for body in bodies_conditions if body[0] == name), None)
+    v_z = next((body[6] for body in bodies_conditions if body[0] == name), None)
 
-    return Body(name, trail_color, mass, x, y, z, speed_x, speed_y, speed_z)
+    return Body(name, trail_color, mass, x, y, z, v_x, v_y, v_z)
 
 
 #  Choice of objects that are properly registered in both files.
@@ -63,4 +56,4 @@ BODIES = []
 
 #  Formation of BODIES list (doesnt get changed later).
 for body_name in registered_objects_names:
-    BODIES.append(Register_body(body_name))
+    BODIES.append(register_body(body_name))
